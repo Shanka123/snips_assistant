0468b8af80dd9cd67461a053bb4fd6f6f064a520
shanka <shankasubhra@gmail.com>
2019-01-27 15:56:57 +0800

    Dialogue commit

--- V2_action-how-are-you.py
+++ V2_action-how-are-you.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 from hermes_python.hermes import Hermes
 
-INTENT_HOW_ARE_YOU = "bezzam:how_are_you"
-INTENT_GOOD = "bezzam:feeling_good"
-INTENT_BAD = "bezzam:feeling_bad"
-INTENT_ALRIGHT = "bezzam:feeling_alright"
+INTENT_HOW_ARE_YOU = "shanka1997:how_are_you"
+INTENT_GOOD = "shanka1997:feeling_good"
+INTENT_BAD = "shanka1997:feeling_bad"
+INTENT_ALRIGHT = "shanka1997:feeling_alright"
 
 INTENT_FILTER_FEELING = [INTENT_GOOD, INTENT_BAD, INTENT_ALRIGHT]
 
@@ -21,25 +21,25 @@ def main():
 
 def how_are_you_callback(hermes, intent_message):
     session_id = intent_message.session_id
-    response = "I'm doing great. How about you?"
+    response = "I'm doing great. Tell me how are you doing?"
     hermes.publish_continue_session(session_id, response, INTENT_FILTER_FEELING)
 
 
 def feeling_good_callback(hermes, intent_message):
     session_id = intent_message.session_id
-    response = "That's awesome! I'm happy to hear that."
+    response = "That's  is what i wanted to hear! I'm very glad. "
     hermes.publish_end_session(session_id, response)
 
 
 def feeling_bad_callback(hermes, intent_message):
     session_id = intent_message.session_id
-    response = "Sorry to hear that. I hope you feel better soon."
+    response = "Oh my god, that is bad to hear."
     hermes.publish_end_session(session_id, response)
 
 
 def feeling_alright_callback(hermes, intent_message):
     session_id = intent_message.session_id
-    response = "That's cool."
+    response = "That's cool!! Enjoy."
     hermes.publish_end_session(session_id, response)
 
 
--- action-how-are-you.py
+++ action-how-are-you.py
@@ -3,17 +3,43 @@
 from hermes_python.hermes import Hermes
 
 INTENT_HOW_ARE_YOU = "shanka1997:how_are_you"
+INTENT_GOOD = "shanka1997:feeling_good"
+INTENT_BAD = "shanka1997:feeling_bad"
+INTENT_ALRIGHT = "shanka1997:feeling_alright"
+
+INTENT_FILTER_FEELING = [INTENT_GOOD, INTENT_BAD, INTENT_ALRIGHT]
 
 
 def main():
     with Hermes("localhost:1883") as h:
         h.subscribe_intent(INTENT_HOW_ARE_YOU, how_are_you_callback) \
+         .subscribe_intent(INTENT_GOOD, feeling_good_callback) \
+         .subscribe_intent(INTENT_BAD, feeling_bad_callback) \
+         .subscribe_intent(INTENT_ALRIGHT, feeling_alright_callback) \
          .start()
 
 
 def how_are_you_callback(hermes, intent_message):
     session_id = intent_message.session_id
-    response = "I'm doing great."
+    response = "I'm doing great. Tell me how are you doing?"
+    hermes.publish_continue_session(session_id, response, INTENT_FILTER_FEELING)
+
+
+def feeling_good_callback(hermes, intent_message):
+    session_id = intent_message.session_id
+    response = "That's  is what i wanted to hear! I'm very glad. "
+    hermes.publish_end_session(session_id, response)
+
+
+def feeling_bad_callback(hermes, intent_message):
+    session_id = intent_message.session_id
+    response = "Oh my god, that is bad to hear."
+    hermes.publish_end_session(session_id, response)
+
+
+def feeling_alright_callback(hermes, intent_message):
+    session_id = intent_message.session_id
+    response = "That's cool!! Enjoy."
     hermes.publish_end_session(session_id, response)
 
 
